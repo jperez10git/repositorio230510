775b3f7806529b9b11a06e3fa0125b6c3108bb55
Profesor-B16 <Profesor-B16@B07-PC01>
2023-05-10 21:41:41 +0200

    version 3

--- GitPrueba3/src/Prueba1git.java
+++ GitPrueba3/src/Prueba1git.java
@@ -6,6 +6,8 @@ public class Prueba1git {
 	public static void main(String[] args) {
 		System.out.println("prueba v1");
 		System.out.println("prueba v2");
+		System.out.println("prueba v3");
+		
 	}	
 
 }
